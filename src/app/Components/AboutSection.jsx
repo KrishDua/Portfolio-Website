97a4540f62d53088f70f483fdd059b1f172b6e97
Krish Dua <Krishdua312@gmail.com>
2025-04-19 22:45:20 +0530

    first commit

--- src/app/Components/AboutSection.jsx
+++ src/app/Components/AboutSection.jsx
@@ -45,7 +45,7 @@ export default function AboutSection() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4">
             <AboutCard heading = "Who Am I?" bullets = {whoami} />
             <AboutCard heading = "What Drives Me?" bullets = {whatdrivesme} />
-            <AboutCard heading = "Achievement" bullets = {achievement} />
+            <AboutCard heading = "Achievements" bullets = {achievement} />
         </div>
     </section>
   )
